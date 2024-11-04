0914575d4bacc789c5b7cddbf15ff0ddc61d0fe9
adrianopiacentini <adrianopiacentini03@gmail.com>
2024-11-04 17:06:09 +0100

    Esercizio email

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,16 +1,16 @@
 const userLogin = prompt('Inserisci la tua e-mail')
-const credentials = ['email@protonmail.com','email@gmail.com','email@smth.me']
+const credentials = ['email@protonmail.com','email@gmail.com','email@smth.me'];
 
-let login;
+let isFound = false;
 
-for(let i = 0; i < credentials.length - 1; i++) {
+for(let i = 0; i <= credentials.length - 1; i++) {
     currEmail = credentials[i]
     if (currEmail === userLogin) {
-        console.log('Il login è stato effettuato correttamente!')
-    } else {
-        console.log('La mail inserita è errata, controlla e riprova!')
+        isFound = true;
     }
 }
-
-// console.log('Il login è stato effettuato correttamente!')
-// console.log('La mail inserita è errata, controlla e riprova!')
+if (isFound === true) {
+    console.log(`L'email ${userLogin} è stata trovata, Login effettuato!`)
+} else {
+    console.error(`Non puoi accedere!`);
+}
